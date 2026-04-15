4c1d0e441ea2e7ebda684b2c6091f3a00a3ff343
Aliet Exposito Garcia <aliet.exposito@gmail.com>
2026-04-15 01:28:54 -0400

    feat: implement level completion progress bar with percent for arcade mode
    Introduces a visual progress bar specifically designed for arcade mode levels that use a blips win condition.
    Implements a vertical bar that fills from the bottom to the top as the player advances in the level.

--- game/colors.lua
+++ game/colors.lua
@@ -47,4 +47,12 @@ local Colors = {
   murky_blue = { 0.063, 0.09, 0.176 },
 }
 
+function Colors.lerp(c1, c2, t)
+  local r = c1[1] + (c2[1] - c1[1]) * t
+  local g = c1[2] + (c2[2] - c1[2]) * t
+  local b = c1[3] + (c2[3] - c1[3]) * t
+  local a = (c1[4] or 1) + ((c2[4] or 1) - (c1[4] or 1)) * t
+  return { r, g, b, a }
+end
+
 return Colors
